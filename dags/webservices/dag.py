9569725c1c38fb0a5d70f2bc770666faab97e26c
kien8995 <trandongkien.sine@gmail.com>
2019-09-21 20:39:16 +0700

    update

--- dags/webservices/dag.py
+++ dags/webservices/dag.py
@@ -1,10 +1,8 @@
 from airflow import DAG
-from airflow.operators.python_operator import PythonOperator, ShortCircuitOperator
+from airflow.operators.dummy_operator import DummyOperator
 from datetime import datetime, timedelta
 from google.cloud import bigquery
-from dags.webservices.operators.flt_bl import CheckFLT_BLOperator, InsertFLT_BLOperator
-import requests
-from utils.json import is_jsonable
+from dags.webservices.flt_bl import FLT_BLTask
 
 project_id = 'dwh-demo'
 dataset_id = 'staging_flight'
@@ -46,89 +44,15 @@ def query_stackoverflow(**context):
         print("{} : {} views".format(row.url, row.view_count))
     return results.total_rows
 
-def get_json_data(**context):
-    run_date = context['yesterday_ds_nodash']
-    URL = "http://10.223.19.8:1988/api/partner/aitsgetreportodsbyday?startday={fromDate}&endday={toDate}".format(fromDate=run_date, toDate=run_date)
-    response = requests.get(url = URL).json()
-    if is_jsonable(response):
-        context['task_instance'].xcom_push(key='json_data', value=response)
-        return True
-    return False
+with dag:
+    start = DummyOperator(
+        task_id='start'
+    )
 
-def insert_data_to_table(**context):
-    json_data = context['task_instance'].xcom_pull(task_ids='get_flt_bl', key='json_data')
+    end = DummyOperator(
+        task_id='end'
+    )
 
-    results = []
-    for x in json_data:
-        tmp_dict = {}
-        tmp_dict['FlightDate'] = datetime.strptime(x['FlightDate'], '%Y%m%d').strftime('%Y-%m-%d')
-        tmp_dict['CarrierCode'] = x['CarrierCode']
-        tmp_dict['FlightNumber'] = x['FlightNumber'].strip()
-        tmp_dict['DepartureDate'] = datetime.strptime(x['DepartureDate'], '%Y%m%d').strftime('%Y-%m-%d')
-        tmp_dict['ArrivalDate'] = datetime.strptime(x['ArrivalDate'], '%Y%m%d').strftime('%Y-%m-%d')
-        tmp_dict['Leg_STD'] = x['Leg_STD']
-        tmp_dict['Leg_STA'] = x['Leg_STA']
-        tmp_dict['ADT'] = x['ADT']
-        tmp_dict['CHD'] = x['CHD']
-        tmp_dict['INF'] = x['INF']
-        tmp_dict['DepartureStation'] = x['DepartureStation']
-        tmp_dict['ArrivalStation'] = x['ArrivalStation']
-        tmp_dict['Segment'] = x['Segment']
-        tmp_dict['Capacity'] = x['Capacity']
-        tmp_dict['BD'] = x['BD']
-        tmp_dict['NS'] = x['NS']
-        tmp_dict['PaxRev'] = x['PaxRev']
-        tmp_dict['BaggageAmount'] = float(x['BaggageAmount'])
-        tmp_dict['Other_Rev'] = float(x['Other_Rev'])
-        tmp_dict['CargoRev'] = float(x['CargoRev'])
-        tmp_dict['VCost'] = float(x['VCost'])
-        tmp_dict['FCost'] = float(x['FCost'])
-        tmp_dict['TotalCost'] = float(x['TotalCost'])
-        tmp_dict['QTQN'] = x['QTQN']
-        tmp_dict['FLS_TYPE'] = x['FLS_TYPE']
-        results.append(tmp_dict)
-
-    client = bigquery.Client()
-    table = client.get_table("{}.{}.{}".format(project_id,dataset_id,table_id))
-    errors = client.insert_rows(table, results)
-
-    if errors == []:
-        print("Insert successfully!")
-    else:
-        print("Insert error.")
-
-# run_this = PythonOperator(
-#     task_id='query_stackoverflow',
-#     provide_context=True,
-#     python_callable=query_stackoverflow,
-#     dag=dag,
-# )
-
-t1 = CheckFLT_BLOperator(
-    task_id="check_table_flt_bl",
-    table_id="{}.{}.{}".format(project_id,dataset_id,table_id),
-    dag=dag,
-)
-
-t2 = ShortCircuitOperator(
-    task_id="get_flt_bl",
-    provide_context=True,
-    python_callable=get_json_data,
-    dag=dag,
-)
-
-t3 = PythonOperator(
-    task_id="insert_table_flt_bl",
-    provide_context=True,
-    python_callable=insert_data_to_table,
-    dag=dag,
-)
-
-# t3 = InsertFLT_BLOperator(
-#     task_id="insert_table_flt_bl",
-#     table_id="{}.{}.{}".format(project_id,dataset_id,table_id),
-#     dag=dag,
-# )
-
-t1 >> t2 >> t3
-
+    flt_blTask = FLT_BLTask(dag).tasks()
+    start >> flt_blTask[0]
+    flt_blTask[1] >> end
---
+++ dags/webservices/flt_bl.py
@@ -0,0 +1,141 @@
+from google.cloud import bigquery
+from datetime import datetime
+from airflow.operators.python_operator import PythonOperator, ShortCircuitOperator
+import requests
+from utils.bigquery import check_table_exist
+from utils.json import is_jsonable
+
+class FLT_BLTask(object):
+    def __init__(self, dag):
+        self.dag = dag
+        self.table_id = 'dwh-demo.staging_flight.flt_bl'
+        self.task_id_1 = 'check_table_flt_bl'
+        self.task_id_2 = 'get_flt_bl'
+        self.task_id_3 = 'transform_data'
+        self.task_id_4 = 'insert_data_to_bigquery'
+
+    def tasks(self):
+        t1 = PythonOperator(
+            task_id=self.task_id_1,
+            python_callable=self.__check_table,
+            provide_context=True,
+            dag=self.dag
+        )
+
+        t2 = ShortCircuitOperator(
+            task_id=self.task_id_2,
+            python_callable=self.__get_json_data,
+            provide_context=True,
+            dag=self.dag,
+        )
+
+        t3 = PythonOperator(
+            task_id=self.task_id_3,
+            python_callable=self.__transform_data,
+            provide_context=True,
+            dag=self.dag
+        )
+
+        t4 = PythonOperator(
+            task_id=self.task_id_4,
+            python_callable=self.__insert_data,
+            provide_context=True,
+            dag=self.dag
+        )
+
+        t1 >> t2 >> t3 >> t4
+        return t1, t4
+    
+    def __check_table(self, **context):
+        if not check_table_exist(self.table_id):
+            schema = [
+                bigquery.SchemaField("FlightDate", "DATE", mode="NULLABLE"),
+                bigquery.SchemaField("CarrierCode", "STRING", mode="NULLABLE"),
+                bigquery.SchemaField("FlightNumber", "STRING", mode="NULLABLE"),
+                bigquery.SchemaField("DepartureDate", "DATE", mode="NULLABLE"),
+                bigquery.SchemaField("ArrivalDate", "DATE", mode="NULLABLE"),
+                bigquery.SchemaField("Leg_STD", "STRING", mode="NULLABLE"),
+                bigquery.SchemaField("Leg_STA", "STRING", mode="NULLABLE"),
+                bigquery.SchemaField("ADT", "INTEGER", mode="NULLABLE"),
+                bigquery.SchemaField("CHD", "INTEGER", mode="NULLABLE"),
+                bigquery.SchemaField("INF", "INTEGER", mode="NULLABLE"),
+                bigquery.SchemaField("DepartureStation", "STRING", mode="NULLABLE"),
+                bigquery.SchemaField("ArrivalStation", "STRING", mode="NULLABLE"),
+                bigquery.SchemaField("Segment", "STRING", mode="NULLABLE"),
+                bigquery.SchemaField("Capacity", "INTEGER", mode="NULLABLE"),
+                bigquery.SchemaField("BD", "INTEGER", mode="NULLABLE"),
+                bigquery.SchemaField("NS", "INTEGER", mode="NULLABLE"),
+                bigquery.SchemaField("PaxRev", "FLOAT", mode="NULLABLE"),
+                bigquery.SchemaField("BaggageAmount", "FLOAT", mode="NULLABLE"),
+                bigquery.SchemaField("Other_Rev", "FLOAT", mode="NULLABLE"),
+                bigquery.SchemaField("CargoRev", "FLOAT", mode="NULLABLE"),
+                bigquery.SchemaField("VCost", "FLOAT", mode="NULLABLE"),
+                bigquery.SchemaField("FCost", "FLOAT", mode="NULLABLE"),
+                bigquery.SchemaField("TotalCost", "FLOAT", mode="NULLABLE"),
+                bigquery.SchemaField("QTQN", "STRING", mode="NULLABLE"),
+                bigquery.SchemaField("FLS_TYPE", "STRING", mode="NULLABLE"),
+            ]
+
+            client = bigquery.Client()
+            table = bigquery.Table(self.table_id, schema=schema)
+            table = client.create_table(table)
+            print(
+                "Created table {}.{}.{}".format(table.project, table.dataset_id, table.table_id)
+            )
+
+    def __get_json_data(self, **context):
+        run_date = context['yesterday_ds_nodash']
+        URL = "http://10.223.19.8:1988/api/partner/aitsgetreportodsbyday?startday={fromDate}&endday={toDate}".format(fromDate=run_date, toDate=run_date)
+        response = requests.get(url = URL).json()
+        if is_jsonable(response):
+            context['task_instance'].xcom_push(key='json_data', value=response)
+            return True
+        return False
+
+    def __transform_data(self, **context):
+        data = context['task_instance'].xcom_pull(task_ids=self.task_id_2, key='json_data')
+
+        results = []
+        for x in data:
+            tmp_dict = {}
+            tmp_dict['FlightDate'] = datetime.strptime(x['FlightDate'], '%Y%m%d').strftime('%Y-%m-%d')
+            tmp_dict['CarrierCode'] = x['CarrierCode']
+            tmp_dict['FlightNumber'] = x['FlightNumber'].strip()
+            tmp_dict['DepartureDate'] = datetime.strptime(x['DepartureDate'], '%Y%m%d').strftime('%Y-%m-%d')
+            tmp_dict['ArrivalDate'] = datetime.strptime(x['ArrivalDate'], '%Y%m%d').strftime('%Y-%m-%d')
+            tmp_dict['Leg_STD'] = x['Leg_STD']
+            tmp_dict['Leg_STA'] = x['Leg_STA']
+            tmp_dict['ADT'] = x['ADT']
+            tmp_dict['CHD'] = x['CHD']
+            tmp_dict['INF'] = x['INF']
+            tmp_dict['DepartureStation'] = x['DepartureStation']
+            tmp_dict['ArrivalStation'] = x['ArrivalStation']
+            tmp_dict['Segment'] = x['Segment']
+            tmp_dict['Capacity'] = x['Capacity']
+            tmp_dict['BD'] = x['BD']
+            tmp_dict['NS'] = x['NS']
+            tmp_dict['PaxRev'] = x['PaxRev']
+            tmp_dict['BaggageAmount'] = float(x['BaggageAmount'])
+            tmp_dict['Other_Rev'] = float(x['Other_Rev'])
+            tmp_dict['CargoRev'] = float(x['CargoRev'])
+            tmp_dict['VCost'] = float(x['VCost'])
+            tmp_dict['FCost'] = float(x['FCost'])
+            tmp_dict['TotalCost'] = float(x['TotalCost'])
+            tmp_dict['QTQN'] = x['QTQN']
+            tmp_dict['FLS_TYPE'] = x['FLS_TYPE']
+            results.append(tmp_dict)
+        
+        context['task_instance'].xcom_push(key='transform_data', value=results)
+        return results
+
+    def __insert_data(self, **context):
+        data = context['task_instance'].xcom_pull(task_ids=self.task_id_3, key='transform_data')
+
+        client = bigquery.Client()
+        table = client.get_table(self.table_id)
+        errors = client.insert_rows(table, data)
+
+        if errors == []:
+            print("Insert successfully!")
+        else:
+            print("Insert error.")
