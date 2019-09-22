b2bd8296cdddf7e6bc21064e8cc96da0be1df9ec
kien8995 <trandongkien.sine@gmail.com>
2019-09-22 13:08:32 +0700

    update

--- dags/webservices/flt_bl.py
+++ dags/webservices/flt_bl.py
@@ -1,6 +1,7 @@
 from google.cloud import bigquery
 from datetime import datetime
 from airflow.operators.python_operator import PythonOperator, ShortCircuitOperator
+from google.cloud.exceptions import NotFound
 import requests
 from utils.bigquery import check_table_exist
 from utils.json import is_jsonable
@@ -12,7 +13,8 @@ class FLT_BLTask(object):
         self.task_id_1 = 'check_table_flt_bl'
         self.task_id_2 = 'get_flt_bl'
         self.task_id_3 = 'transform_data'
-        self.task_id_4 = 'insert_data_to_bigquery'
+        self.task_id_4 = 'delete_data'
+        self.task_id_5 = 'insert_data_to_bigquery'
 
     def tasks(self):
         t1 = PythonOperator(
@@ -36,15 +38,22 @@ class FLT_BLTask(object):
             dag=self.dag
         )
 
-        t4 = PythonOperator(
+        t4 = ShortCircuitOperator(
             task_id=self.task_id_4,
+            python_callable=self.__delete_data,
+            provide_context=True,
+            dag=self.dag,
+        )
+
+        t5 = PythonOperator(
+            task_id=self.task_id_5,
             python_callable=self.__insert_data,
             provide_context=True,
             dag=self.dag
         )
 
-        t1 >> t2 >> t3 >> t4
-        return t1, t4
+        t1 >> t2 >> t3 >> t4 >> t5
+        return t1, t5
     
     def __check_table(self, **context):
         if not check_table_exist(self.table_id):
@@ -127,6 +136,22 @@ class FLT_BLTask(object):
         
         context['task_instance'].xcom_push(key='transform_data', value=results)
         return results
+
+    def __delete_data(self, **context):
+        client = bigquery.Client()
+        query = """
+            #standardSQL
+            DELETE {table_id} WHERE FlightDate = {date}
+        """.format(table_id=self.table_id, date=context['yesterday_ds_nodash'])
+
+        try:
+            query_job = client.query(query)
+            query_job.result()
+            return True
+        except Exception as e:
+            print(e)
+
+        return False
 
     def __insert_data(self, **context):
         data = context['task_instance'].xcom_pull(task_ids=self.task_id_3, key='transform_data')
--- utils/bigquery.py
+++ utils/bigquery.py
@@ -12,3 +12,26 @@ def check_table_exist(table_id):
         # ...do some processing ...
         print('Whoops! Table {} doesn\'t exist here! Ref: {}'.format(table_id, error.response))
         return False
+
+def create_dataset(dataset_id):
+    bigquery_client = bigquery.Client()
+    dataset_ref = bigquery_client.dataset(dataset_id)
+
+    try:
+        bigquery_client.get_dataset(dataset_ref)
+    except NotFound:
+        dataset = bigquery.Dataset(dataset_ref)
+        dataset = bigquery_client.create_dataset(dataset)
+        print('Dataset {} created.'.format(dataset.dataset_id))
+
+def exist_record(query):
+    bigquery_client = bigquery.Client()
+
+    try:
+        query_job = bigquery_client.query(query)
+        is_exist = len(list(query_job.result())) >= 1
+        return is_exist
+    except Exception as e:
+        print(e)
+
+    return False
